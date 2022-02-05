5bf21545020d91d01d7af9979a34a6efd0b5565f
Luminatione <01171749@pw.edu.pl>
2022-02-05 16:49:18 +0100

    generated key are now stored

--- controllers/getKey.js
+++ controllers/getKey.js
@@ -1,6 +1,22 @@
 const {v4: uuid} = require('uuid');
+const hash = require('sha1');
+
+const insertIntoDb = require('../utility/db').insertIntoDb;
+const logger = require('../logger').logger;
 
 const getKey = (req, res) => {
-    res.status(200).json({status: 'OK', key: uuid()});
+    const key = uuid();
+    const {keyType} = req.body;
+    try {
+        if(keyType !== 'in' && keyType !== 'out'){
+            throw 'invalid key type';
+        }
+        await insertIntoDb(`${keytype}keys`, hash(key));
+    }
+    catch(err) {
+        logger.log(err);
+        res.status(400).json({success: false});
+    }
+    res.status(200).json({success: true, key: key});
 };
 module.exports = getKey;
