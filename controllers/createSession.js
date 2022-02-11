97a447e2b86810ea8f7a139bac6701d39390fa6e
Luminatione <01171749@pw.edu.pl>
2022-02-11 12:59:24 +0100

    sessions id are encrypted instead of hashed and can be returned via getSessions

--- controllers/createSession.js
+++ controllers/createSession.js
@@ -1,23 +1,32 @@
-const {v4: uuid} = require('uuid');
+const { v4: uuid } = require('uuid');
 const hash = require('sha1');
 const insertIntoDb = require('../Utility/db/remoteCLIDbWrapper').insertIntoDb;
+const crypto = require('crypto');
 
 const ctx = require('../env');
+const { logger } = require('../logger');
 
+const encryptSessionId = (sessionId, inKey) => {
+    const initVector = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv(ctx.sessionEncryptionAlgorithm, inKey.substr(0, 32), initVector);
+    const encryptedSessionId = cipher.update(sessionId, 'utf8', 'hex') + cipher.final('hex');
+    return {encryptedSessionId: encryptedSessionId, initVector: initVector.toString('hex')};
+}
 
 const createSession = async (req, res) => {
-    try{
-        const {inKey} = req.body;
-        if(!inKey)
-        {
+    try {
+        const { inKey } = req.body;
+        if (!inKey) {
             throw 'No valid in key provided';
         }
         const sessionId = uuid();
-        await insertIntoDb(ctx.dbSessionsCollectionName, { sessionId: hash(sessionId), inKey: hash(inKey) });
-        res.status(200).json({success: true, sessionId: sessionId});
+        const {encryptedSessionId, initVector} = encryptSessionId(sessionId, inKey);
+        await insertIntoDb(ctx.dbSessionsCollectionName, { sessionId:encryptedSessionId, initVector: initVector, inKey: hash(inKey) });
+        res.status(200).json({ success: true, sessionId: sessionId });
     }
-    catch(err) {
-        res.status(400).json({success: false});
+    catch (err) {
+        logger.error(err.stack);
+        res.status(400).json({ success: false });
     }
 };
 
--- controllers/getSessions.js
+++ controllers/getSessions.js
@@ -1,15 +1,24 @@
 const logger = require('../logger').logger;
 const findAll = require('../utility/db/remoteCLIDbWrapper').findAll;
 const ctx = require('../env');
+const crypto = require('crypto');
 
 const hash = require('sha1');
+
+const decryptSessionId = (session, inKey) => {
+    const decipher = crypto.createDecipheriv(ctx.sessionEncryptionAlgorithm, inKey.substr(0, 32), Buffer.from(session.initVector, 'hex'));
+    const decryptedSessionId = decipher.update(session.sessionId, 'hex', 'utf8') + decipher.final('utf8');
+    return decryptedSessionId;
+}
 
 const getSessions = async (req, res) => {
     try {
         const {inKey} = req.body;
-        const response = await findAll(ctx.dbSessionsCollectionName, {inKey: hash(inKey)});
+        let response = await findAll(ctx.dbSessionsCollectionName, {inKey: hash(inKey)});
+        response = response.map(session => decryptSessionId(session, inKey));           
         res.status(200).json({sessions: response});
     }
+
     catch (err) {
         logger.error(err.stack);
         res.status(400).json({success: false});
